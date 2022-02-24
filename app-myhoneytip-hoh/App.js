cf5c68aa67279baf63ece109ec54d9effaa82fe0
dlghgus5656 <82634597+dlghgus5656@users.noreply.github.com>
2022-02-24 20:08:37 +0900

    week2
    App development study week2
    (리액트 네이티브, Expo, JSX문법, 스타일속성, View,Text, ScrollView, Button, TouchableOpacity, lmage(assets,외부), flex, flexDirection, justifyContent, alignitems)

---
+++ app-myhoneytip-hoh/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.containerOne}></View>
+      <View style={styles.containerTwo}>
+        <View style={styles.innerOne}></View>
+        <View style={styles.innerTwo}>
+          <Text>!!컨텐츠!!</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  containerOne: {
+    flex: 1,
+    backgroundColor: "red",
+  },
+  containerTwo: {
+    flex: 2,
+    flexDirection: "row",
+    backgroundColor: "yellow",
+  },
+  innerOne: {
+    flex: 1,
+    backgroundColor: "blue",
+  },
+  innerTwo: {
+    flex: 4,
+    flexDirection: "column",
+    alignItems: "flex-start",
+    backgroundColor: "orange",
+  },
+});
