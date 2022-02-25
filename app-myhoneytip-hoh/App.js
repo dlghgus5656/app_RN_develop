186de8524eb20ed81f7330a1548ce50d1021fd8d
dlghgus5656 <82634597+dlghgus5656@users.noreply.github.com>
2022-02-25 22:33:15 +0900

    week2 done
    StyleSheet, Text, View, Image, TouchableOpacity, ScrollView, 모듈 시스템, map을 이용한 JSX 안에서 반복 적용, 간단한 페이지 제작

--- app-myhoneytip-hoh/App.js
+++ app-myhoneytip-hoh/App.js
@@ -1,41 +1,8 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import MainPage from "./pages/MainPage";
+import AboutPage from "./pages/AboutPage";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.containerOne}></View>
-      <View style={styles.containerTwo}>
-        <View style={styles.innerOne}></View>
-        <View style={styles.innerTwo}>
-          <Text>!!컨텐츠!!</Text>
-        </View>
-      </View>
-    </View>
-  );
+  // return <MainPage />;
+  return <AboutPage />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  containerOne: {
-    flex: 1,
-    backgroundColor: "red",
-  },
-  containerTwo: {
-    flex: 2,
-    flexDirection: "row",
-    backgroundColor: "yellow",
-  },
-  innerOne: {
-    flex: 1,
-    backgroundColor: "blue",
-  },
-  innerTwo: {
-    flex: 4,
-    flexDirection: "column",
-    alignItems: "flex-start",
-    backgroundColor: "orange",
-  },
-});
---
+++ app-myhoneytip-hoh/pages/AboutPage.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+
+export default function AboutPage() {
+  const aboutImage =
+    "https://firebasestorage.googleapis.com/v0/b/sparta-image.appspot.com/o/lecture%2FaboutImage.png?alt=media&token=13e1c4f6-b802-4975-9773-e305fc7475c4";
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>이것은 타이틀입니다 여러분!!</Text>
+
+      <View style={styles.middle_container}>
+        <Image style={styles.middle_img} source={{ uri: aboutImage }} />
+        <Text style={styles.mid_text1}>이것은 텍스트입니다 여러분!!</Text>
+        <Text style={styles.mid_text2}>두번째 텍스트입니다 여러분!!</Text>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.button_text}>여러분의 인스타계정</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#00498c",
+  },
+  title: {
+    textAlign: "center",
+    fontSize: 30,
+    marginTop: 70,
+    width: 400,
+    color: "white",
+    fontWeight: "600",
+  },
+  middle_container: {
+    width: 350,
+    height: 540,
+    alignSelf: "center",
+    backgroundColor: "white",
+    marginTop: 60,
+    borderRadius: 30,
+  },
+  middle_img: {
+    width: 170,
+    height: 170,
+    alignSelf: "center",
+    borderRadius: 30,
+    marginTop: 70,
+  },
+  mid_text1: {
+    textAlign: "center",
+    fontSize: 20,
+    fontWeight: "700",
+    paddingTop: 10,
+  },
+  mid_text2: {
+    textAlign: "center",
+    marginTop: 20,
+    fontSize: 15,
+    fontWeight: "700",
+  },
+  button: {
+    backgroundColor: "orange",
+    alignSelf: "center",
+    width: 180,
+    height: 65,
+    borderRadius: 20,
+    padding: 20,
+    marginTop: 20,
+  },
+  button_text: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "700",
+  },
+});
